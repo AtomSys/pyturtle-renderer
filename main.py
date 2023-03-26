66cfe8e1fc48cdf4886bf1b1ab514341b0261c5f
AtomSys <zcrenshaw1@icloud.com>
2023-03-26 16:06:57 -0400

    Added Function draw_rectangle for non-equilateral rectangles.

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ def draw_polygon(sides,size):
     for x in range(0,int(sides)):
         forward(int(size))
         right(360 / int(sides))
+def draw_rectangle(s1,s2):
+    for x in range(0,2):
+        right(90)
+        forward(int(s1))
+        right(90)
+        forward(int(s2))
